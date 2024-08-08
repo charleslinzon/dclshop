0e2a833ecc6e9184f2dbc117bd0d0359ece0f0d6
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-08 16:38:47 -0600

    Create Route and Login Register Setup - Part 4

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
     return (
@@ -11,7 +13,28 @@ const Register = () => {
                     <form />
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor="name">Name</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='name' placeholder='Name' id='name required' />
+                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='name' placeholder='Name' id='name' required />
+                        </div>
+
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <label htmlFor="name">Email</label>
+                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='email' placeholder='Email' id='email' required />
+                        </div>
+
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <label htmlFor="name">Password</label>
+                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="password" name='password' placeholder='Password' id='password' required />
+                        </div>
+
+                        <div className='flex items-center w-full gap-3 mb-3'>
+                            <input className='w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-50 focus:ring-blue-500' type="checkbox" name="checkbox" id="checkbox" />
+                            <label htmlFor="checkbox">I agree to privacy policy & terms.</label>
+                        </div>
+                        <button className='bg-slate-800 w-full hover:shadow-blue-300/50  hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign Up</button>
+
+                        <div className='flex items-center mb-3 gap-3 justify-center'>
+                            <p>Already have and account? <Link className='font-bold' to="/login">Sign In</Link></p>
+
                         </div>
                 </div>
 
