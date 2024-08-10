f5eda42a0bb907137bad34a30dc12d3f253ad1b8
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-09 19:54:08 -0600

    Error and Notification Message - Part 2

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -3,13 +3,15 @@ import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
-    async(info) => {
+    async(info,{rejectWithValue, fulfillWithValue}) => {
         console.log(info)
         try{
             const {data} = await api.post(`/admin-login`, info, {withCredentials: true})
-            console.log(data)
+            //console.log(data)
+            return fulfillWithValue(data)
         } catch (error) {
-            console.log(error.response.data)    
+            //console.log(error.response.data)
+            return rejectWithValue(error.response.data)    
         }
     }
 
@@ -24,6 +26,9 @@ export const authReducer = createSlice({
         userInfo: ''
     },
     reducers : {
+        messageClear : (state,_) => {
+            state.errorMessage = ""
+        }
 
     },
     extraReducers: (builder) => {
@@ -31,8 +36,13 @@ export const authReducer = createSlice({
         .addCase(admin_login.pending, (state, { payload }) => {
             state.loader = true;
         })
+        .addCase(admin_login.rejected, (state, { payload }) => {
+            state.loader = false;
+            state.errorMessage = payload.error
+        })
 
     }
 })
 
+export const{messageClear} = authReducer.actions
 export default authReducer.reducer
