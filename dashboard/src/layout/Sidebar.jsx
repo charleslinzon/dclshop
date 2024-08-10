d99635b22d93fa829043d26d62dd5942a300de40
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-10 15:51:20 -0600

    Manage Project Navigation Design - Part 4

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { getNav } from '../navigation';
+import { BiLogOut } from "react-icons/bi";
 
 const Sidebar = () => {
 
@@ -38,6 +39,13 @@ const Sidebar = () => {
                             
                         </li>)
                     }
+
+                    <li>
+                        <button className = 'text-[#030811] font-bold duration-200  px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1' >
+                        <span><BiLogOut /></span>
+                        <span>Logout</span>
+                        </button>
+                    </li>
                 </ul>
             </div>
         </div>
