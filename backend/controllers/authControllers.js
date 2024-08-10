c75fc2eb5daa3b8b8987115fe533d3f597e57238
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-09 18:13:18 -0600

    Admin Login Page Setup - Part 3: Creating Token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,7 @@
 const adminModel = require('../models/adminModel')
 const { responseReture } = require('../utilities/response')
 const bcrypty = require('bcrypt')
+const { createToken } = require('../utilities/tokenCreate')
 
 class authControllers{
     admin_login = async(req,res) => {
@@ -12,7 +13,18 @@ class authControllers{
 
             if (admin) {
                 const match = await bcrypty.compare(password, admin.password)
-                console.log(match)
+                //console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : admin.id,
+                        role : admin.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000) })
+                        responseReture(res,200,{token, message: "Login Success"})
+                } else {
+                    responseReture(res,404,{error: "Password not correct"})
+                }
 
             } else {
                 responseReture(res,404,{error: "Email not found"})
