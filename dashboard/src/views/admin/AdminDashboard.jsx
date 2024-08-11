f5769158ad2be18b8ac60b85160732461561e41f
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-10 23:38:30 -0600

    Manage Project Dashboard Design - Part 3

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -47,7 +47,25 @@ const AdminDashboard = () => {
             },
             legend : {
                 position : 'top'
-            }
+            },
+            responsive : [
+                {
+                    breakpoint : 655,
+                    yaxis : {
+                        categories : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec' ]
+                    },
+                    options : {
+                        plotOptions : {
+                            bar : {
+                                horizontal : true
+                            } 
+                        },
+                        chart : { 
+                            height : "550px"
+                        }
+                    }
+                }
+            ]
         }
     }
 
