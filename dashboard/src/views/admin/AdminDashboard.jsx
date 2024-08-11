773c60a13bd94bb8f7146dc6f8847ee55122e065
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-10 23:26:42 -0600

    Manage Project Dashboard Design - Part 2

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -3,8 +3,54 @@ import { MdCurrencyExchange } from "react-icons/md";
 import { MdProductionQuantityLimits } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
 import { FaCartArrowDown } from "react-icons/fa";
+import Chart from 'react-apexcharts'
 
 const AdminDashboard = () => {
+
+    const state = {
+        series : [
+            {
+                name : "Orders",
+                data : [23,34,45,56,76,56,78,67,56,23,45,54]
+            },
+            {
+                name : "Revenue",
+                data : [23,34,85,26,86,56,88,37,26,32,56,57]
+            },
+            {
+                name : "Sellers",
+                data : [93,44,55,26,76,26,18,97,76,89,76,56]
+            },
+        ],
+        options : {
+            colors : ['#181ee8', '#00e396', '#feb019'],
+            plotOptions : {
+                radius : 30 
+            },
+            chart : {
+                background : 'transparent',
+                foreColor : '#d0d2d6' 
+            },
+            dataLabels : {
+                enabled : false
+            },
+            stroke : {
+                show : true,
+                curve : ['smooth','straight','stepline'],
+                lineCap : 'butt',
+                colors : '#f0f0f0',
+                width : .5,
+                dashArray : 0
+            },
+            xaxis : {
+                categories : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec' ]
+            },
+            legend : {
+                position : 'top'
+            }
+        }
+    }
+
     return (
         <div className='px-2 md:px-7 py-5 '>
             <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7' >
@@ -49,6 +95,17 @@ const AdminDashboard = () => {
                     </div>
                 </div>
             </div>
+
+            <div className='w-full flex flex-wrap mt-7' >
+                <div className='w-full lg:w-7/12 lg:pr-3'>
+                    <div className='w-full bg-[#6a5fdf] p-4 rounded-md'>
+                        <Chart options={state.options} series={state.series} type='bar' height={350} />
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
     );
 };
