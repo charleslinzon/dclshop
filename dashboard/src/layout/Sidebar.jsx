68b5f05de1103951ede89175c736ece561a2f5f4
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-10 21:14:55 -0600

    Manage Project Header Design - Part 3

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = ({showSidebar, setShowSidebar}) => {
 
     return (
     <div>
-        <div onClick={()=> setShowSidebar(false)} className={`flex duration-200 ${!showSidebar ? 'invisible' : 'visible'} w-screen h-screen bg-[#8cbce780] top-0 left-0 z-10`}>
+        <div onClick={()=> setShowSidebar(false)} className={`fixed duration-200 ${!showSidebar ? 'invisible' : 'visible'} w-screen h-screen bg-[#8cbce780] top-0 left-0 z-10`}>
 
         </div>
 
