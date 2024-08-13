77d37e17bf48bc81a2012acd8578e2cbd9538d5f
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-12 18:27:59 -0600

    Sellers Page Design

--- dashboard/src/views/admin/Orders.jsx
+++ dashboard/src/views/admin/Orders.jsx
@@ -14,7 +14,6 @@ const Orders = () => {
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
                 <div className='flex justify-between items-center'>
-
                     <select onChange={(e) => setParPage(parseInt(e.target.value)) }className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
                         <option value="5">5</option>
                         <option value="10">10</option>
