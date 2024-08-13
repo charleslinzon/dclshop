649f2fad9a5ffe6e781782e028d56b8f9adf5b34
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-12 22:40:11 -0600

    Deactive Sellers Page Design

--- dashboard/src/views/admin/DeactivateSellers.jsx
+++ dashboard/src/views/admin/DeactivateSellers.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Pagination from './../Pagination';
+import Pagination from '../Pagination';
 import { FaEye } from "react-icons/fa";
 
-const Sellers = () => {
+const DeactivateSellers = () => {
     
     const [currentPage, setCurrentPage] = useState(1)
     const [searchValue, setSearchValue] = useState('')
@@ -13,7 +13,7 @@ const Sellers = () => {
     return (
         <div className='px-2 lg:px-7 pt-5'>
             <div className='text-[20px] font-bold mb-3 '>
-                <h1>Seller</h1>
+                <h1>Deactivate Seller</h1>
             </div>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
                 <div className='flex justify-between items-center'>
@@ -33,11 +33,9 @@ const Sellers = () => {
                                     <th scope='col' className='py-3 px-4'>No</th>
                                     <th scope='col' className='py-3 px-4'>Image</th>
                                     <th scope='col' className='py-3 px-4'>Name</th>
-                                    <th scope='col' className='py-3 px-4'>Shop Name</th>
-                                    <th scope='col' className='py-3 px-4'>Payment Status</th>
                                     <th scope='col' className='py-3 px-4'>Email</th>
-                                    <th scope='col' className='py-3 px-4'>Division</th>
-                                    <th scope='col' className='py-3 px-4'>District</th>
+                                    <th scope='col' className='py-3 px-4'>Payment Status</th>
+                                    <th scope='col' className='py-3 px-4'>Status</th>
                                     <th scope='col' className='py-3 px-4'>Action</th>
                                 </tr>
                             </thead>
@@ -49,11 +47,9 @@ const Sellers = () => {
                                         <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
                                             <img className='w-[45px] h-[45px]' src={`http://localhost:3000/images/category/${d}.jpg`} alt="" /></td>
                                             <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Charles Dy</td>
-                                            <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>DCL Shop</td>
-                                            <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'><span>Pending</span></td>
                                             <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>dycharles@gmail.com</td>
-                                            <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Lakeside</td>
-                                            <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Southern</td>
+                                            <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'><span>Pending</span></td>
+                                            <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'><span>Deactive</span></td>
                                         <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
                                             <div className='flex justify-start items-center gap-4'>
                                                 <Link className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'><FaEye /></Link>
@@ -78,4 +74,4 @@ const Sellers = () => {
     );
 };
 
-export default Sellers;
+export default DeactivateSellers;
