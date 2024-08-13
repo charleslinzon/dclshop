6eb46883466a4e4fb09d01c871b031ebc714a674
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-12 22:01:46 -0600

    Payment Request Page Design - Part 2

--- dashboard/src/views/admin/PaymentRequest.jsx
+++ dashboard/src/views/admin/PaymentRequest.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { FixedSizeList as List } from 'react-window';
 
+function handleOnWheel({ deltaY }) {
+    console.log('handleOnWheel', deltaY)
+}
+
+const outerElementType = forwardRef((props, ref) => (
+    <div ref={ref} onWheel={handleOnWheel} {...props} />
+));
+
+
+const array = [1,2,3,4,5,6,7,8,9]
+
 const PaymentRequest = () => {
+
+    const Row = ({ index, style }) => {
+        return (
+            <div style={style} className='flex text-sm text-white font-medium'>
+                <div className='w-[25%] p-2 whitespace-nowrap'>{index + 1}</div>
+                <div className='w-[25%] p-2 whitespace-nowrap'>$5453</div>
+                <div className='w-[25%] p-2 whitespace-nowrap'><span className='py-[1px] px-[5px bg-slate-300 text-blue-500 rounded-md text-sm]'>Pending</span></div>
+                <div className='w-[25%] p-2 whitespace-nowrap'> 25 Dec 2023 </div>
+                <div className='w-[25%] p-2 whitespace-nowrap'><button className='bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 px-3 py-[2px] cursor-pointer text-white rounded-sm text-sm'>Confirm</button></div>
+            </div>            
+        )
+    } 
+
     return (
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
-                <h2 className='text-xl font-medium pb-5 text-[#d0d2d6]'>Withdrawwal Request</h2>
+                <h2 className='text-xl font-medium pb-5 text-[#d0d2d6]'>Withdrawal Request</h2>
                 <div className='w-full'>
                     <div className='w-full overflow-x-auto'>
                         <div className='flex bg-[#a7a3de] uppercase text-xs font-bold min-w-[34-px] rounded-md'>
@@ -16,6 +40,20 @@ const PaymentRequest = () => {
                         <div className='w-[25%] p-2'>Action</div>
                         </div>
                     </div>
+                    {
+                        <List
+                        style={{minWidth : '340px'}}
+                        className='List'
+                        height={350}
+                        itemCount={10}
+                        itemSize={35}
+                        outerElementType={outerElementType}
+                        >
+                        
+                            {Row}
+                        
+                        </List>
+                    }
                 </div>
             </div>            
         </div>
