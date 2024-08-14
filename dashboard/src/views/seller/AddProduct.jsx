fa04e09584dd8ebd9bdd676a5292ddb3e512de72
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-13 22:43:17 -0600

    Add Product Page Design - Part 8

--- dashboard/src/views/seller/AddProduct.jsx
+++ dashboard/src/views/seller/AddProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaRegImages } from "react-icons/fa";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const AddProduct = () => {
     const categorys = [
@@ -93,6 +94,14 @@ const AddProduct = () => {
         }
     }
 
+    const removeImage = (i) => {
+        const filterImage = images.filter((img, index) => index !== i)
+        const filterImageUrl = imageShow.filter((img, index) => index !== i)
+
+        setImages(filterImage)
+        setImageShow(filterImageUrl)
+    }
+
     return (
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
@@ -110,7 +119,7 @@ const AddProduct = () => {
                             </div>
 
                             <div className='flex flex-col w-full gap-1'>
-                                <label htmlFor="brand">Product Name</label>
+                                <label htmlFor="brand">Product Brand</label>
                                 <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.brand} type="text" name='brand' id='brand' placeholder='Brand Name'/>
                             </div>                            
                         </div>
@@ -162,13 +171,16 @@ const AddProduct = () => {
                             <textarea  className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.description}  name='description' id='description' placeholder='description' cols="10" rows="4"></textarea>
                         </div>
 
-                        <div className='grid lg:grid-cols-4 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 gap-3 w-full text-[#d0d2d6] mb-4' >
+                        <div className='grid lg:grid-cols-4 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 gap-5 w-full text-[#d0d2d6] mb-4' >
                             {
                                 imageShow.map((img,i) => <div className='h-[180px] relative'>
                                     <label htmlFor="{i}">
                                         <img className='w-full h-full rounded-sm' src={img.url} alt="" />
                                     </label>
                                     <input onChange={(e) => changeImage(e.target.files[0],i)} type="file" id='{i}' className='hidden' />
+                                    <span onClick={() => removeImage(i)} className='p-2 z-10 cursor-pointer bg-slate-700 hover:shadow-lg hover:shadow-slate-400/50 text-white absolute top-1 right-1 rounded-full'><IoMdCloseCircleOutline /></span>
+
+
                                 </div>)
                             }
 
@@ -178,6 +190,11 @@ const AddProduct = () => {
                             </label>
                             <input className='hidden' onChange={imageHandle} multiple type="file" id='image' />                            
                         </div>
+
+                        <div className='flex'>
+                            <button className='bg-red-500 hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-1.5 my-2'>Add Product</button>
+                        </div>                       
+
                     </form>
                 </div>
             </div>            
