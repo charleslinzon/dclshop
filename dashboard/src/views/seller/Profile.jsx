11232b02416c57aaadc792a5b1eef2c6972b4ccc
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-14 16:36:11 -0600

    Seller Profile Page Design - Part 5

--- dashboard/src/views/seller/Profile.jsx
+++ dashboard/src/views/seller/Profile.jsx
@@ -78,7 +78,7 @@ const Profile = () => {
                         
                         <div className='px-0 md:px-5 py-2'>
                             {
-                                userInfo && <form>
+                                !userInfo ? <form>
                                     <div className='flex flex-col w-full gap-1 mb-2'>
                                         <label htmlFor="shop">Shop Name</label>
                                         <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" name='shopName' id='shop' placeholder='Shop Name'/>
@@ -98,14 +98,57 @@ const Profile = () => {
 
                                     <button className='bg-red-500 hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-1.5 my-2'>Save Changes</button>
 
-                                </form>
+                                </form> :
+                                    
+                                    <div className='flext justify-between text-sm flex-cols gap-2 p-4 bg-slate-800 rounded-md relative'>
+                                        <span className='p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50 absolute right-2 top-2 cursor-pointer'><FaEdit /></span>
+
+                                        <div className='flex gap-2 mb-2'>
+                                            <span>Shop Name : </span>
+                                            <span>DCL Shop</span>
+                                        </div>
+                                        <div className='flex gap-2 mb-2'>
+                                            <span>Division :</span>
+                                            <span>Millwoods</span>
+                                        </div>
+                                        <div className='flex gap-2 mb-2'>
+                                            <span>District : </span>
+                                            <span>North</span>
+                                        </div>
+                                        <div className='flex gap-2 mb-2'>
+                                            <span>Sub District : </span>
+                                            <span>One</span>
+                                        </div>                                    
+                                    </div>                                
                             }
                         </div>
                     </div>
                 </div>
 
                 <div className='w-full md:w-6/12'>
+                    <div className='w-full pl-0 md:pl-7 mt-6 md:mt-0'>
+                        <div className='bg-[#6a5fdf] rounded-md text-[#d0d2d6] p-4'>
 
+                            <h1 className='text-[#d0d2d6] text-lg mb-3 font-semibold'>Change Password</h1>
+                            
+                            <form>
+                                <div className='flex flex-col w-full gap-1 mb-2'>
+                                    <label htmlFor="shop">Email</label>
+                                    <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="email" name='email' id='email' placeholder='Email'/>
+                                </div>
+                                <div className='flex flex-col w-full gap-1 mb-2'>
+                                    <label htmlFor="o_password">Old Password</label>
+                                    <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="password" name='o_password' id='o_password' placeholder='Old Password'/>
+                                </div>
+                                <div className='flex flex-col w-full gap-1 mb-2'>
+                                    <label htmlFor="n_password">New Password</label>
+                                    <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="password" name='n_password' id='n_password' placeholder='New Password'/>
+                                </div>
+
+                                <button className='bg-red-500 hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-1.5 my-2'>Save Changes</button>
+                            </form>
+                        </div>                    
+                    </div>
                 </div>                
             </div>            
         </div>
