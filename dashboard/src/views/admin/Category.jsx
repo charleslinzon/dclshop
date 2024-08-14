931969c1b5bb8e6f5acc67e6bf551c463dbe7e8e
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-13 23:20:25 -0600

    All Product Page Design - Part 1

--- dashboard/src/views/admin/Category.jsx
+++ dashboard/src/views/admin/Category.jsx
@@ -21,6 +21,7 @@ const Category = () => {
             <div className='flex flex-wrap w-full'>
                 <div className='w-full lg:w-7/12'>
                     <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
+                        
                         <div className='flex justify-between items-center'>
                         <select onChange={(e) => setParPage(parseInt(e.target.value)) }className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
                             <option value="5">5</option>
