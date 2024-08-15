05f46ab325fe7c76d421d089a3ecd8225c3d0056
charleslinzon <dycharleslinzon.ca@gmail.com>
2024-08-14 20:35:38 -0600

    Product Edit Page Design - Part 2

--- dashboard/src/views/seller/EditProduct.jsx
+++ dashboard/src/views/seller/EditProduct.jsx
@@ -66,40 +66,11 @@ const EditProduct = () => {
     const [images, setImages] = useState([])
     const [imageShow, setImageShow] = useState([])
 
-    const imageHandle = (e) => {
-        const files = e.target.files
-        const length = files.length
-        
-        if (length > 0) {
-            setImages([...images, ...files])
-            let imageUrl = []
-            for (let i = 0; i < length; i++) {
-                imageUrl.push({url: URL.createObjectURL(files[i])})            
-            }
-            setImageShow([...imageShow, ...imageUrl])           
-            
-        }
-    }
-
-    const changeImage = (img, index) => {
-        if (img) {
-            let tempUrl = imageShow
-            let tempImages = images
-
-            tempImages[index] = img
-            tempUrl[index] = {url : URL.createObjectURL(img)}
-
-            setImageShow([...tempUrl])
-            setImages([...tempImages])
-        }
-    }
-
-    const removeImage = (i) => {
-        const filterImage = images.filter((img, index) => index !== i)
-        const filterImageUrl = imageShow.filter((img, index) => index !== i)
-
-        setImages(filterImage)
-        setImageShow(filterImageUrl)
+    const changeImage = (img, files) => {
+        if (files.length > 0) {
+            console.log(img)
+            console.log(files[0])
+        } 
     }
 
     useEffect(() => {
@@ -117,7 +88,6 @@ const EditProduct = () => {
             'http://localhost:3000/images/demo.jpg',
             'http://localhost:3000/images/seller.png'
         ])
-
 
     },[])
 
@@ -192,20 +162,14 @@ const EditProduct = () => {
 
                         <div className='grid lg:grid-cols-4 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 gap-5 w-full text-[#d0d2d6] mb-4' >
                             {
-                                imageShow.map((img,i) => <div className='h-[180px] relative'>
-                                    <label htmlFor="{i}">
-                                        <img className='w-full h-full rounded-sm' src={img} alt="" />
+                                imageShow.map((img, i) => <div>
+                                    <label htmlFor={i}>
+                                        <img src={img} alt="" />
                                     </label>
-                                    <input onChange={(e) => changeImage(e.target.files[0],i)} type="file" id='{i}' className='hidden' />
-                                    <span onClick={() => removeImage(i)} className='p-2 z-10 cursor-pointer bg-slate-700 hover:shadow-lg hover:shadow-slate-400/50 text-white absolute top-1 right-1 rounded-full'><IoMdCloseCircleOutline /></span>
-                                </div>)
-                            }
+                                    <input onChange={(e) => changeImage(img, e.target.files)} className='hidden' type="file" id="i" />
 
-                            <label className='flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed hover:border-red-500 w-full text-[#d0d2d6]' htmlFor="image">
-                                <span><FaRegImages /></span>
-                                <span>Select Image</span>
-                            </label>
-                            <input className='hidden' onChange={imageHandle} multiple type="file" id='image' />                            
+                                </div>)
+                            }                          
                         </div>
 
                         <div className='flex'>
